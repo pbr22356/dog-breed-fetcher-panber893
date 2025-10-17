a2bb0fad8c848d0991f33b37bf7065342279bca0
brpan <panber893@gmail.com>
2025-10-16 23:47:21 -0400

    feat: implement caching logic in CachingBreedFetcher (refs #2)

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -22,45 +22,7 @@ public class Main {
      * @param breed the name of the dog breed
      * @param breedFetcher the breedFetcher to use
      * @return the number of sub breeds. Zero should be returned if there are no sub breeds
-     * returned by the fetcher
-     */
-    public static int getNumberOfSubBreeds(String breed, BreedFetcher fetcher) {
-        if (breed == null) throw new IllegalArgumentException("breed must not be null");
-        if (fetcher == null) throw new IllegalArgumentException("breedFetcher must not be null");
-        try {
-            return fetcher.getSubBreeds(breed).size();
-        } catch (BreedFetcher.BreedNotFoundException e) {
-            return 0;
-        }
-    }
-
-
-
-}package dogapi;
-
-import java.util.List;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String breed = "hound";
-        BreedFetcher fetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
-
-        int result = getNumberOfSubBreeds(breed, fetcher);
-        System.out.println(breed + " has " + result + " sub breeds");
-
-        breed = "cat";
-        result = getNumberOfSubBreeds(breed, fetcher);
-        System.out.println(breed + " has " + result + " sub breeds");
-    }
-
-    /**
-     * Return the number of sub breeds that the given dog breed has according to the
-     * provided fetcher.
-     * @param breed the name of the dog breed
-     * @param breedFetcher the breedFetcher to use
-     * @return the number of sub breeds. Zero should be returned if there are no sub breeds
-     * returned by the fetcher
+     * returned by the fetc
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher fetcher) {
         if (breed == null) throw new IllegalArgumentException("breed must not be null");
